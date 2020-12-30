e885377a8781bffda8e1dc070a0290885081e718
silveracy <silveracy@naver.com>
2020-12-30 10:09:08 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-C55W3N858V`,
       },
     },
     `gatsby-plugin-feed`,
